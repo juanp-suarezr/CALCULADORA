555109c6ffc1d1fa349a9d7f2014eb29f9c9499d
juanp-suarezr <86664381+juanp-suarezr@users.noreply.github.com>
2021-07-17 23:48:13 -0500

    Add files via upload
    Interfaz Simple de una calculadora, le falta los eventos

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package CALCULADORA;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Ventana vc = new Ventana();
+
+        vc.setVisible(true);
+    }
+    
+}
